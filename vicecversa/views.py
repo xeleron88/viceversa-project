db5fd72ca913f33a942c6100aba2c788e1cb49cb
Евгений Астафуров <xelseo@MacBook-Pro-Evgenij.local>
2021-01-03 16:22:10 +0300

    last edits

--- vicecversa/views.py
+++ vicecversa/views.py
@@ -6,5 +6,6 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	reversed_text = str(reversed_text)
-	return render(request, 'reverse.html', {'usertext':reversed_text, 'len_words':(len(set(reversed_text.split())))})
+	newlist = reversed_text.split()
+	number_of_words = len(newlist)
+	return render(request, 'reverse.html', {'usertext':reversed_text, 'len_words':number_of_words})
